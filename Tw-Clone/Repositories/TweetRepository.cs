4b0d40860500008f91055b674102313e50fc482b
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2023-11-03 10:29:34 -0300

    feat: adding tweet and user repository

---
+++ Tw-Clone/Repositories/TweetRepository.cs
@@ -0,0 +1,27 @@
+﻿using Tw_Clone.Models;
+
+namespace Tw_Clone.Repositories
+{
+
+    public interface ITweetRepository : IRepository<Tweet>
+    {
+        Task<Tweet> Update(Tweet entity);
+    }
+    public class TweetRepository : Repository<Tweet>, ITweetRepository
+    {
+
+        private readonly TwcloneContext _db;
+
+        public TweetRepository(TwcloneContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<Tweet> Update(Tweet entity)
+        {
+           _db.Tweets.Update(entity);
+            await Save(); 
+            return entity;
+        }
+    }
+}
---
+++ Tw-Clone/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Tw_Clone.Models;
+
+namespace Tw_Clone.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> Update(User entity);
+    }
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+
+
+        private readonly TwcloneContext _db;
+
+        public UserRepository(TwcloneContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<User> Update(User entity)
+        {
+           _db.Users.Add(entity);
+            await Save();
+            return entity;
+        }
+    }
+}
